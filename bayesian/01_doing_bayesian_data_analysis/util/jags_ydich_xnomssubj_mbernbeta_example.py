024a17116f10d9eb0cea1748f4d0dc065b707840
Pablo Suau <pablosuau@gmail.com>
2020-05-23 14:49:11 +0100

    feat: first version of exercise 8 1

---
+++ bayesian/01_doing_bayesian_data_analysis/util/jags_ydich_xnomssubj_mbernbeta_example.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from jags_ydich_xnomssubj_mbernbeta import gen_mcmc
+
+# Example for jags_ydich_xnomssubj_mbernbeta.py
+
+def jags_ydich_xnomssubj_mbernbeta_example(data_csv = 'z6n8z2n7.csv'):
+	my_data = pd.read_csv(data_csv)
+	
+	mcmc_trace = gen_mcmc(data = my_data, num_saved_steps = 50000)
+
+'''
+# Load the relevant model into R's working memory:
+source("Jags-Ydich-XnomSsubj-MbernBeta.R")
+#------------------------------------------------------------------------------- 
+# Optional: Specify filename root and graphical format for saving output.
+# Otherwise specify as NULL or leave saveName and saveType arguments 
+# out of function calls.
+fileNameRoot = "Jags-Ydich-XnomSsubj-MbernBeta-" 
+graphFileType = "eps" 
+#------------------------------------------------------------------------------- 
+# Generate the MCMC chain:
+mcmcCoda = genMCMC( data=myData , numSavedSteps=50000 , saveName=fileNameRoot )
+#------------------------------------------------------------------------------- 
+# Display diagnostics of chain, for specified parameters:
+parameterNames = varnames(mcmcCoda) # get all parameter names
+for ( parName in parameterNames ) {
+  diagMCMC( codaObject=mcmcCoda , parName=parName , 
+                saveName=fileNameRoot , saveType=graphFileType )
+}
+#------------------------------------------------------------------------------- 
+# Get summary statistics of chain:
+summaryInfo = smryMCMC( mcmcCoda , compVal=NULL , #rope=c(0.45,0.55) ,
+                        compValDiff=0.0 , #ropeDiff = c(-0.05,0.05) ,
+                        saveName=fileNameRoot )
+# Display posterior information:
+plotMCMC( mcmcCoda , data=myData , compVal=NULL , #rope=c(0.45,0.55) ,
+          compValDiff=0.0 , #ropeDiff = c(-0.05,0.05) ,
+          saveName=fileNameRoot , saveType=graphFileType )
+#------------------------------------------------------------------------------- 
+'''
