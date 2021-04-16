93e32b7e7d4babaf6318dcb8d4aa11d47fad83e1
Pablo Suau <pablosuau@gmail.com>
2021-04-16 15:24:18 +0100

    feat: translated first script in chapter 10

--- bayesian/01_doing_bayesian_data_analysis/util/jags_ydich_xnom1subj_mbernbetamodelcomp.py
+++ bayesian/01_doing_bayesian_data_analysis/util/jags_ydich_xnom1subj_mbernbetamodelcomp.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pymc3 as pm
+from math import ceil
+from dbda2e_utilities import diag_mcmc, plot_post
 
 # THE DATA
 n = 9
@@ -24,73 +26,43 @@ with pm.Model() as model:
 
   # RUN THE CHAINS
   # Test
-  trace = pm.sample(chains = 4, draws = 1000)
-  import arviz as av
-  av.plot_trace(trace)
-  print(av.summary(trace))
-'''
+  n_chains = 4
+  num_saved_steps = 50000
+  thin_steps = 1
+  # RUN THE CHAINS - some of the parameters in the original R code do not translate
+  # well to the PyMC3's MCMC algorithm (thin_Steps, burn_in_steps)
+  n_per_chain = ceil((num_saved_steps * thin_steps) / float(n_chains))
+  trace = pm.sample(chains = n_chains, draws = n_per_chain)
 
-#------------------------------------------------------------------------------
-# RUN THE CHAINS.
+  parameter_names = ['theta', 'm']
+  for par_name in parameter_names:
+    diag_mcmc(trace, par_name = par_name)
+  
+  #import arviz as av
+  #av.plot_trace(trace)
+  #print(av.summary(trace))
 
-parameters = c("theta","m") 
-adaptSteps = 1000             # Number of steps to "tune" the samplers.
-burnInSteps = 1000           # Number of steps to "burn-in" the samplers.
-nChains = 4                   # Number of chains to run.
-numSavedSteps=50000          # Total number of steps in chains to save.
-thinSteps=1                   # Number of steps to "thin" (1=keep every step).
-nPerChain = ceiling( ( numSavedSteps * thinSteps ) / nChains ) # Steps per chain.
-# Create, initialize, and adapt the model:
-jagsModel = jags.model( "TEMPmodel.txt" , data=dataList , # inits=initsList , 
-                        n.chains=nChains , n.adapt=adaptSteps )
-# Burn-in:
-cat( "Burning in the MCMC chain...\n" )
-update( jagsModel , n.iter=burnInSteps )
-# The saved MCMC chain:
-cat( "Sampling final MCMC chain...\n" )
-codaSamples = coda.samples( jagsModel , variable.names=parameters , 
-                            n.iter=nPerChain , thin=thinSteps )
-# resulting codaSamples object has these indices: 
-#   codaSamples[[ chainIdx ]][ stepIdx , paramIdx ]
+  # EXAMINE THE RESULTS
 
-save( codaSamples , file=paste0(fileNameRoot,"Mcmc.Rdata") )
+  # Convert trace samples to matrix objects for easier handling
+  m = trace['m']
+  theta = trace['theta']
 
-#------------------------------------------------------------------------------- 
-# Display diagnostics of chain:
+  # Compute the proportion of m at each index value
+  pm1 = np.sum(m == 0) / len(m)
+  pm2 = 1 - pm1
+ 
+  # Extract theta values for each model index
+  thetam1 = theta[m == 0]
+  thetam2 = theta[m == 1]
 
-parameterNames = varnames(codaSamples) # get all parameter names
-for ( parName in parameterNames ) {
-  diagMCMC( codaSamples , parName=parName ,
-            saveName=fileNameRoot , saveType="eps" )
-}
-
-#------------------------------------------------------------------------------
-# EXAMINE THE RESULTS.
-
-# Convert coda-object codaSamples to matrix object for easier handling.
-mcmcMat = as.matrix( codaSamples , chains=TRUE )
-m = mcmcMat[,"m"]
-theta = mcmcMat[,"theta"]
-
-# Compute the proportion of m at each index value:
-pM1 = sum( m == 1 ) / length( m )
-pM2 = 1 - pM1
-
-# Extract theta values for each model index:
-thetaM1 = theta[ m == 1 ]
-thetaM2 = theta[ m == 2 ]
-
-# Plot histograms of sampled theta values for each model,
-# with pM displayed.
-openGraph(width=7,height=5)
-par( mar=0.5+c(3,1,2,1) , mgp=c(2.0,0.7,0) )
-layout( matrix(c(1,1,2,3),nrow=2,byrow=FALSE) , widths=c(1,2) )
-plotPost( m , breaks=seq(0.9,2.1,0.2) , cenTend="mean" , xlab="m" , main="Model Index" )
-plotPost( thetaM1 , 
-          main=bquote( theta*" when m=1" * " ; p(m=1|D)" == .(signif(pM1,3)) ) , 
-          cex.main=1.75 , xlab=bquote(theta) , xlim=c(0,1) )
-plotPost( thetaM2 , 
-          main=bquote( theta*" when m=2" * " ; p(m=2|D)" == .(signif(pM2,3)) ) , 
-          cex.main=1.75 , xlab=bquote(theta) , xlim=c(0,1) )
-saveGraph( file=paste0(fileNameRoot,"Post") , type="eps" )
-'''
+  # Plot histograms of sampled theta values for each model
+  plot_post(m, cen_tend = 'mean', xlab = 'm', main = 'model index')
+  plot_post(thetam1, 
+            main = 'theta when m=1 ; p(m=1|D) = %.3f' % round(pm1, 3),
+            xlab = 'theta',
+            xlim = [0, 1])
+  plot_post(thetam2, 
+            main = 'theta when m=2 ; p(m=2|D) = %.3f' % round(pm2, 3),
+            xlab = 'theta',
+            xlim = [0, 1])
